6fdc31e466794648c453630fea74bd2172015074
Erik Lima <eriklim@hotmail.com>
2021-03-11 17:00:05 -0300

    terceiro-commit-curso-typescript-11-03-2021

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -24,3 +24,44 @@ console.log(typeof hobbies);
 hobbies = [100, 200, 300];
 // hobbies = 100
 console.log(hobbies);
+// tuplas
+var endereco = ["Av Principal", 99, ""];
+console.log(endereco);
+endereco = ["Rua Importante", 1260, "Bloco C"];
+console.log(endereco);
+// enums
+var Cor;
+(function (Cor) {
+    Cor[Cor["Cinza"] = 0] = "Cinza";
+    Cor[Cor["Verde"] = 100] = "Verde";
+    Cor[Cor["Azul"] = 10] = "Azul";
+    Cor[Cor["Laranja"] = 11] = "Laranja";
+    Cor[Cor["Amarelo"] = 12] = "Amarelo";
+    Cor[Cor["Vermelho"] = 100] = "Vermelho";
+})(Cor || (Cor = {}));
+var minhaCor = Cor.Verde;
+console.log(minhaCor);
+console.log(Cor.Azul);
+console.log(Cor.Laranja, Cor.Amarelo);
+console.log(Cor.Verde, Cor.Vermelho);
+// any
+var carro = 'BMW';
+console.log(carro);
+carro = { marca: 'BMW', ano: 2019 };
+console.log(carro);
+// funções
+function retornaMeuNome() {
+    // return minhaIdade
+    return nome;
+}
+console.log(retornaMeuNome());
+function digaOi() {
+    console.log('Oi');
+    // return minhaIdade
+}
+digaOi();
+function multiplicar(numA, numB) {
+    return numA * numB;
+}
+// console.log(multiplcar(2, 'Bia'))
+console.log(multiplicar(4.7, 9));
