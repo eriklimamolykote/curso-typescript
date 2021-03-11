a3eae52b3060afce008275590b9ab8e35bc55220
Erik Lima <eriklim@hotmail.com>
2021-03-11 16:41:56 -0300

    segundo-commit-curso-typescript-11-03-2021

---
+++ tipos/tipos.js
@@ -0,0 +1,26 @@
+"use strict";
+// string
+var nome = 'João';
+console.log(nome);
+// nome = 28
+// numbers
+var idade = 27;
+// idade = 'Ana'
+idade = 27.5;
+console.log(idade);
+// boolean
+var possuiHobbies = false;
+// possuiHobbies = 1
+console.log(possuiHobbies);
+// tipos explícitos
+var minhaIdade;
+minhaIdade = 27;
+console.log(typeof minhaIdade);
+// minhaIdade = '27'
+// array
+var hobbies = ["Cozinhar", "Praticar Esportes"];
+console.log(hobbies[0]);
+console.log(typeof hobbies);
+hobbies = [100, 200, 300];
+// hobbies = 100
+console.log(hobbies);
