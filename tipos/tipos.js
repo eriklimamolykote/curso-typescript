8af2f5a62317be06d09b128b2bc8e87d0c305b6f
Erik Lima <eriklim@hotmail.com>
2021-03-12 16:50:26 -0300

    commit-curso-type-script-12-03-2021

--- tipos/tipos.js
+++ tipos/tipos.js
@@ -65,3 +65,121 @@ function multiplicar(numA, numB) {
 }
 // console.log(multiplcar(2, 'Bia'))
 console.log(multiplicar(4.7, 9));
+// tipo função
+var calculo;
+// calculo = digaOi
+// calculo()
+// calculo = {}
+calculo = multiplicar;
+console.log(calculo(5, 6));
+// objetos
+var usuario = {
+    nome: 'João',
+    idade: 27
+};
+console.log(usuario);
+// usuario = {
+//     nome: 'Maria',
+//     idade: 31
+// }
+usuario = {
+    idade: 31,
+    nome: 'Maria',
+};
+console.log(usuario);
+var funcionario = {
+    supervisores: ['Erik', 'Bruno', 'Lucas'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário!';
+        }
+    }
+};
+var funcionario2 = {
+    supervisores: ['Bia', 'Jessica', 'Bruna'],
+    baterPonto: function (horario) {
+        if (horario <= 8) {
+            return 'Ponto normal';
+        }
+        else {
+            return 'Fora do horário!';
+        }
+    }
+};
+console.log(funcionario.supervisores);
+console.log(funcionario.baterPonto(8));
+console.log(funcionario.baterPonto(9));
+// funcionario = {}
+// Union Types
+var nota = 10;
+console.log("Minha nota \u00E9 " + nota + "!");
+nota = '10';
+console.log("Minha nota \u00E9 " + nota + "!");
+// nota = true
+// Checando tipos
+var valor = 30;
+if (typeof valor === "number") {
+    console.log("É um number!");
+}
+else {
+    console.log(typeof valor);
+}
+// never
+function falha(msg) {
+    throw new Error(msg);
+}
+var produto = {
+    nome: 'Sabão',
+    preco: 4,
+    validarProduto: function () {
+        if (!this.nome || this.nome.trim().length == 0) {
+            falha('Precisa ter um nome');
+        }
+        if (this.preco <= 0) {
+            falha('Preço inválido!');
+        }
+    }
+};
+produto.validarProduto();
+var altura = 12;
+// altura = null
+var alturaOpcional = 12;
+alturaOpcional = null;
+var contato1 = {
+    nome: 'Fulano',
+    tel1: '98765432',
+    tel2: null
+};
+console.log(contato1.nome);
+console.log(contato1.tel1);
+console.log(contato1.tel2);
+var podeSerNulo = null;
+// podeSerNulo = 12
+console.log(podeSerNulo);
+// podeSerNulo = 'abc'
+console.log(podeSerNulo);
+// Desafio - Transformar código JS em TS
+var contaBancaria = {
+    saldo: 3456,
+    depositar: function (valor) {
+        this.saldo += valor;
+    }
+};
+var correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
+var contaBancariaCliente = {
+    saldo: 3456,
+    depositar: function (valor) {
+        this.saldo += valor;
+    }
+};
+correntista.contaBancaria.depositar(3000);
+console.log(correntista);
--- tipos/tipos.ts
+++ tipos/tipos.ts
@@ -80,3 +80,178 @@ function multiplicar(numA: number, numB: number): number {
 
 // console.log(multiplcar(2, 'Bia'))
 console.log(multiplicar(4.7, 9))
+
+// tipo função
+let calculo: (x: number, y: number) => number
+// calculo = digaOi
+// calculo()
+// calculo = {}
+
+calculo = multiplicar
+console.log(calculo(5, 6))
+
+// objetos
+let usuario: { nome: string, idade: number } = {
+    nome: 'João',
+    idade: 27
+}
+
+console.log(usuario)
+// usuario = {
+    //     nome: 'Maria',
+    //     idade: 31
+    // }
+    
+    usuario = {
+        idade: 31,
+        nome: 'Maria',
+    }
+    console.log(usuario)
+
+    // Desafio
+    /*
+
+        Criar um objeto funcionário com:
+            - Array de strings com os nomes dos supervisores
+            - Função de bater ponto que recebe a hora (número)
+                e retorna uma string
+                -> Ponto normal (<=8)
+                -> Fora do horário (>8) 
+    */
+// Alias
+
+type Funcionario = {
+    supervisores: string[],
+    baterPonto: (horas: number) => string
+}
+
+let funcionario: Funcionario = {
+    supervisores: ['Erik', 'Bruno', 'Lucas'],
+    baterPonto(horario: number): string {
+        if (horario <= 8) {
+            return 'Ponto normal'
+        } else {
+            return 'Fora do horário!'
+        }
+    }
+}
+
+let funcionario2: Funcionario = {
+    supervisores: ['Bia', 'Jessica', 'Bruna'],
+    baterPonto(horario: number): string {
+        if (horario <= 8) {
+            return 'Ponto normal'
+        } else {
+            return 'Fora do horário!'
+        }
+    }
+}
+
+console.log(funcionario.supervisores)
+console.log(funcionario.baterPonto(8))
+console.log(funcionario.baterPonto(9))
+
+// funcionario = {}
+
+// Union Types
+let nota: any = 10
+console.log(`Minha nota é ${nota}!`)
+nota = '10'
+console.log(`Minha nota é ${nota}!`)
+// nota = true
+
+// Checando tipos
+let valor = 30
+
+if (typeof valor === "number") {
+    console.log("É um number!")
+} else {
+    console.log(typeof valor)
+}
+
+// never
+function falha(msg: string): never {
+    throw new Error(msg)
+}
+
+const produto = {
+    nome: 'Sabão',
+    preco: 4,
+    validarProduto() {
+        if (!this.nome || this.nome.trim().length == 0) {
+            falha('Precisa ter um nome')
+        }
+        if (this.preco <= 0) {
+            falha('Preço inválido!')
+        }
+    }
+}
+
+produto.validarProduto()
+
+let altura = 12
+// altura = null
+
+let alturaOpcional: null | number = 12
+alturaOpcional = null
+
+type Contato = {
+    nome: string,
+    tel1: string,
+    tel2: string | null
+}
+
+const contato1: Contato = {
+    nome: 'Fulano',
+    tel1: '98765432',
+    tel2: null
+}
+
+console.log(contato1.nome)
+console.log(contato1.tel1)
+console.log(contato1.tel2)
+
+let podeSerNulo = null
+// podeSerNulo = 12
+console.log(podeSerNulo)
+// podeSerNulo = 'abc'
+console.log(podeSerNulo)
+
+// Desafio - Transformar código JS em TS
+let contaBancaria = {
+    saldo: 3456,
+    depositar(valor) {
+        this.saldo += valor
+    }
+}
+
+let correntista = {
+    nome: 'Ana Silva',
+    contaBancaria: contaBancaria,
+    contatos: ['34567890', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
+
+// Resposta Desafio - Transformar código JS em TS
+type ContaBancaria = {
+    saldo: number,
+    depositar: (valor: number) => void
+}
+
+let contaBancariaCliente: ContaBancaria = {
+    saldo: 3456,
+    depositar(valor: number) {
+        this.saldo += valor
+    }
+}
+
+type Correntista = {
+    nome: string,
+    contaBancariaCliente: ContaBancaria,
+    contatos: ['34567899', '98765432']
+}
+
+correntista.contaBancaria.depositar(3000)
+console.log(correntista)
