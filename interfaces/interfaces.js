e8e60cdcdfe1d2dcee6ff1c6587e8134ec68986c
Erik Lima <eriklim@hotmail.com>
2021-03-23 15:13:27 -0300

    primeiro-commit-curso-typescript-23-03-2021

--- interfaces/interfaces.js
+++ interfaces/interfaces.js
@@ -33,4 +33,46 @@ meuCliente.nome = 'Han';
 saudarComOla(meuCliente);
 meuCliente.saudar('Solo');
 console.log(meuCliente.ultimaCompra);
+let potencia;
+potencia = function (base, exp) {
+    return Array(exp).fill(base).reduce((t, a) => t * a);
+};
+console.log(potencia(3, 10));
+console.log(Math.pow(3, 10));
+console.log(Math.pow(3, 10));
+class RealA {
+    a() { }
+}
+class RealAB {
+    a() { }
+    b() { }
+}
+class RealABC {
+    a() { }
+    b() { }
+    c() { }
+}
+class AbstrataABD {
+    a() { }
+    b() { }
+}
+Object.prototype.log = function () {
+    console.log(this.toString());
+};
+const x = 2;
+const y = 3;
+const z = 4;
+x.log();
+y.log();
+z.log();
+const cli = {
+    nome: 'Pedro',
+    toString() {
+        return this.nome;
+    }
+};
+console.log(x);
+console.log(y);
+console.log(z);
+cli.log();
 //# sourceMappingURL=interfaces.js.map
--- interfaces/interfaces.ts
+++ interfaces/interfaces.ts
@@ -43,3 +43,81 @@ meuCliente.nome = 'Han'
 saudarComOla(meuCliente)
 meuCliente.saudar('Solo')
 console.log(meuCliente.ultimaCompra)
+
+// Interface Função
+interface FuncaoCalculo {
+    (a: number, b: number): number
+}
+
+let potencia: FuncaoCalculo
+
+potencia = function(base: number, exp: number): number {
+    return Array(exp).fill(base).reduce((t, a) => t * a)
+}
+
+console.log(potencia(3, 10))
+console.log(Math.pow(3, 10))
+console.log(3 **10)
+
+// Herança
+interface A {
+    a(): void
+}
+
+interface B {
+    b(): void
+}
+
+interface ABC extends A, B {
+    c(): void    
+}
+
+class RealA implements A {
+    a(): void {}
+}
+
+class RealAB implements A, B {
+    a(): void {}
+    b(): void {}
+}
+
+class RealABC implements ABC {
+    a(): void {}
+    b(): void {}
+    c(): void {}
+}
+
+abstract class AbstrataABD implements A, B {
+    a(): void {}
+    b(): void {}
+    abstract d(): void
+}
+
+interface Object {
+    log(): void
+}
+
+Object.prototype.log = function() {
+    console.log(this.toString())
+}
+
+const x = 2
+const y = 3
+const z = 4
+
+x.log()
+y.log()
+z.log()
+
+const cli = { 
+    nome: 'Pedro', 
+    toString() { 
+        return this.nome 
+    } 
+}
+
+console.log(x)
+console.log(y)
+console.log(z)
+
+cli.log()
